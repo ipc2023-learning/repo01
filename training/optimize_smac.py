edc482d24a98c408e35b11909f18b9317a296ec0
Czubbi <piotr.gzubicki97@gmail.com>
2023-04-23 13:37:25 +0200

    -implemented target_function and cleanup in optimize_smac.py

--- training/optimize_smac.py
+++ training/optimize_smac.py
@@ -68,60 +68,50 @@ class Eval:
             gnn_threshold=0.5
         ).to_parameter_string()
 
-        # Call([sys.executable, f'{self.SCORPION_PATH}/fast-downward.py','--alias', 'lama',  '--transform-task-options', preprocessor_setting, '--transform-task', f'{self.GNN_REPO_DIR}/src/preprocessor.command', DOMAIN, PROBLEM], "run-scorpion" ,time_limit=300, memory_limit = 4*1024*1024).wait()
         command = [sys.executable, f'{self.SCORPION_PATH}/fast-downward.py', '--alias', 'lama', '--transform-task-options', preprocessor_setting, '--transform-task', f'{self.GNN_REPO_DIR}/src/preprocessor.command', DOMAIN, PROBLEM]
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        
 
-        output, error_output = proc.communicate(timeout=300) # Timeout in seconds TODO: set externally
+        try:
+            output, error_output = proc.communicate(timeout=300) # Timeout in seconds TODO: set externally
 
-        print("Output: ", output.decode())
-        print("Output error: ", error_output.decode())
+            total_time = self.regex_total_time.search(output)
+            num_operators = self.regex_operators.search(output)
+            plan_cost = self.regex_plan_cost.search(output)
 
-        return 10
+            if total_time and num_operators and plan_cost:
+                total_time = float(total_time.group(1))
+                num_operators = float(num_operators.group(1))
+                plan_cost = float(plan_cost.group(1))
+                print (f"Ran {instance} with model settings {model_settings}: time {total_time}, operators {num_operators}, cost {plan_cost}")
+                return num_operators
+            elif self.regex_no_solution.search(output):
+                print (f"Ran {instance} with model settings {model_settings}: not solved due to partial grounding")
+                #print(output.decode())
+                return 10000000
+            else:
+                print (f"WARNING: Ran {instance} with model settings {model_settings}: not solved due to unknown reasons")
 
+                print("Output: ", output.decode())
+                if error_output:
+                    print("Error Output: ", error_output.decode())
+                return 10000000
+        except subprocess.CalledProcessError:
+            print (f"WARNING: Command failed: {' '.join(command)}")
+            print (f"Ran {instance} with model settings {model_settings}: not solved due to crash")
+            return 10000000
 
-        # try:
-        #     output, error_output = proc.communicate(timeout=300) # Timeout in seconds TODO: set externally
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            print (f"RRan {instance} with model settings {model_settings}: not solved due to time limit")
+            return 10000000
 
-        #     total_time = self.regex_total_time.search(output)
-        #     num_operators = self.regex_operators.search(output)
-        #     plan_cost = self.regex_plan_cost.search(output)
+        except:
+            print (f"Error: Command failed: {' '.join(command)}")
 
-        #     if total_time and num_operators and plan_cost:
-        #         total_time = float(total_time.group(1))
-        #         num_operators = float(num_operators.group(1))
-        #         plan_cost = float(plan_cost.group(1))
-        #         print (f"Ran {instance} with queue {config['queue_type']} and model {config_name}: time {total_time}, operators {num_operators}, cost {plan_cost}")
-        #         return num_operators
-        #     elif self.regex_no_solution.search(output):
-        #         print (f"Ran {instance} with queue {config['queue_type']} and model {config_name}: not solved due to partial grounding")
-        #         #print(output.decode())
-        #         return 10000000
-        #     else:
-        #         print (f"WARNING: Ran {instance} with queue {config['queue_type']} and model {config_name}: not solved due to unknown reasons")
-
-        #         print("Output: ", output.decode())
-        #         if error_output:
-        #             print("Error Output: ", error_output.decode())
-        #         return 10000000
-        # except subprocess.CalledProcessError:
-        #     print (f"WARNING: Command failed: {' '.join(command)}")
-        #     print (f"Ran {instance} with queue {config['queue_type']} and model {config_name}: not solved due to crash")
-        #     return 10000000
-
-        # except subprocess.TimeoutExpired:
-        #     proc.kill()
-        #     print (f"Ran {instance} with queue {config['queue_type']} and model {config_name}: not solved due to time limit")
-        #     return 10000000
-
-        # except:
-        #     print (f"Error: Command failed: {' '.join(command)}")
-
-        #     print("Output: ", output.decode())
-        #     if error_output:
-        #         print("Error Output: ", error_output.decode())
+            print("Output: ", output.decode())
+            if error_output:
+                print("Error Output: ", error_output.decode())
 
 
 def parse_config(config):
@@ -141,173 +131,12 @@ def parse_config(config):
 
     return model_settings, targetOperators
 
-
-    # def target_function (self, config: Configuration, instance: str, seed: int) -> float:
-    #     if self.candidate_models.is_using_model(config):
-    #         config_name = self.candidate_models.get_unique_model_name(config)
-    #         model_path = os.path.join(self.SMAC_MODELS_DIR, config_name)
-    #         if not os.path.exists(model_path):
-    #             self.candidate_models.copy_model_to_folder(config, model_path, symlink=True)
-    #     else:
-    #         config_name = "---"
-    #         model_path = '.'
-    #         if 'trained' in config['queue_type']:
-    #             return 10000000
-
-    #     extra_parameters = ['--h2-preprocessor', '--alias', config['alias'], '--grounding-queue', config['queue_type']]
-
-
-    #     instance_file = os.path.join(self.instances_dir, instance + ".pddl")
-    #     assert(os.path.exists(instance_file))
-
-    #     command=[sys.executable, f'{self.MY_DIR}/../plan-partial-grounding.py', model_path, self.domain_file, instance_file] + extra_parameters
-    #     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    #     try:
-    #         output, error_output = proc.communicate(timeout=300) # Timeout in seconds TODO: set externally
-
-    #         total_time = self.regex_total_time.search(output)
-    #         num_operators = self.regex_operators.search(output)
-    #         plan_cost = self.regex_plan_cost.search(output)
-
-    #         if total_time and num_operators and plan_cost:
-    #             total_time = float(total_time.group(1))
-    #             num_operators = float(num_operators.group(1))
-    #             plan_cost = float(plan_cost.group(1))
-    #             print (f"Ran {instance} with queue {config['queue_type']} and model {config_name}: time {total_time}, operators {num_operators}, cost {plan_cost}")
-    #             return num_operators
-    #         elif self.regex_no_solution.search(output):
-    #             print (f"Ran {instance} with queue {config['queue_type']} and model {config_name}: not solved due to partial grounding")
-    #             #print(output.decode())
-    #             return 10000000
-    #         else:
-    #             print (f"WARNING: Ran {instance} with queue {config['queue_type']} and model {config_name}: not solved due to unknown reasons")
-
-    #             print("Output: ", output.decode())
-    #             if error_output:
-    #                 print("Error Output: ", error_output.decode())
-    #             return 10000000
-    #     except subprocess.CalledProcessError:
-    #         print (f"WARNING: Command failed: {' '.join(command)}")
-    #         print (f"Ran {instance} with queue {config['queue_type']} and model {config_name}: not solved due to crash")
-    #         return 10000000
-
-    #     except subprocess.TimeoutExpired:
-    #         proc.kill()
-    #         print (f"Ran {instance} with queue {config['queue_type']} and model {config_name}: not solved due to time limit")
-    #         return 10000000
-
-    #     except:
-    #         print (f"Error: Command failed: {' '.join(command)}")
-
-    #         print("Output: ", output.decode())
-    #         if error_output:
-    #             print("Error Output: ", error_output.decode())
-
-
-
-# class CandidateModels:
-#     def __init__(self):
-#         self.sk_models_per_action_schema = defaultdict(lambda : ['none'])
-#         self.good_rules = []
-#         self.bad_rules = []
-
-#     def is_using_model(self, config):
-#         return all ([f'model_{aschema}' in config for aschema in self.sk_models_per_action_schema]) and \
-#             any  ([config[f'model_{aschema}'] != 'none' for aschema in self.sk_models_per_action_schema])
-
-#     def get_unique_model_name(self, config):
-#         assert all([f'model_{aschema}' in config for aschema in self.sk_models_per_action_schema])
-#         prefix = lambda x : "sk" if x.startswith(PREFIX_SK_MODELS) else ("a" if x.endswith(SUFFIX_ALEPH_MODELS) else "")
-#         return "-".join([prefix(config[f'model_{aschema}']) + str(opts.index(config[f'model_{aschema}'])) for aschema, opts in self.sk_models_per_action_schema.items()])
-
-
-#     def load_sk_folder(self, sk_folder):
-#         self.sk_folder = sk_folder
-
-#         sk_models = [name for name in os.listdir(sk_folder) if name.startswith(PREFIX_SK_MODELS)]
-
-#         for model in sk_models:
-#             for n in os.listdir(os.path.join(sk_folder, model)):
-#                 if n == 'relevant_rules':
-#                     continue
-#                 self.sk_models_per_action_schema[n[:-6]].append(model)
-
-
-#     def load_aleph_folder(self, aleph_folder):
-#         self.aleph_folder = aleph_folder
-
-#         aleph_model_filenames = [name for name in os.listdir(aleph_folder) if name.endswith(SUFFIX_ALEPH_MODELS)]
-#         for model_filename in aleph_model_filenames:
-#             with open(os.path.join(aleph_folder, model_filename)) as model_file:
-#                 if 'class_probability' in model_filename:
-#                     for line in model_file.readlines():
-#                         schema = line.split(":-")[ 0].strip()
-#                         self.sk_models_per_action_schema [schema].append(model_filename)
-#                 elif 'good_rules' in model_filename:
-#                     self.good_rules += model_file.readlines()
-#                 elif 'bad_rules' in model_filename:
-#                     self.bad_rules += model_file.readlines()
-#                 else:
-#                     print (f"Warning: ignoring file of unknown type: {model_filename}")
-
-
-#     def copy_model_to_folder(self, config, target_dir, symlink=False ):
-#         if not os.path.exists(target_dir):
-#             os.mkdir(target_dir)
-
-#         collected_relevant_rules = []
-#         collected_aleph_models = []
-#         for aschema in self.sk_models_per_action_schema:
-#             if config[f'model_{aschema}'].startswith(PREFIX_SK_MODELS):
-#                 model_file = os.path.join(self.sk_folder, config[f'model_{aschema}'], aschema + ".model")
-#                 target_file = os.path.join(target_dir, aschema + ".model")
-#                 assert os.path.exists(model_file)
-#                 if symlink:
-#                     os.symlink(model_file, target_file)
-#                 else:
-#                     shutil.copy(model_file, target_file)
-
-#                 with open(os.path.join(self.sk_folder, config[f'model_{aschema}'], 'relevant_rules')) as rfile:
-#                     for line in rfile:
-#                         if line.startswith (aschema + " ("):
-#                             collected_relevant_rules.append(line.strip())
-#             elif config[f'model_{aschema}'].endswith(SUFFIX_ALEPH_MODELS):
-#                 with open(os.path.join(self.aleph_folder, config[f'model_{aschema}'])) as probability_model:
-#                     for line in probability_model.readlines():
-#                         schema = line.split(":-")[0].strip()
-#                         if schema == aschema:
-#                             collected_aleph_models.append(line)
-
-#             else:
-#                 assert config[f'model_{aschema}'] == 'none'
-
-#         if collected_relevant_rules:
-#             with open(os.path.join(target_dir, 'relevant_rules'), 'w') as f:
-#                 f.write('\n'.join(collected_relevant_rules))
-
-#         if collected_aleph_models:
-#             with open(os.path.join(target_dir, 'probability_class.rules'), 'w') as f:
-#                 f.write('\n'.join(collected_aleph_models))
-
-
-
 # Note: default configuration should solve at least 50% of the instances. Pick instances
 # with LAMA accordingly. If we run SMAC multiple times, we can use different instances
 # set, as well as changing the default configuration each time.
 def run_smac(ROOT, DATA_DIR, WORKING_DIR, domain_file, instance_dir, instances_with_features : dict, walltime_limit, n_trials, n_workers):
     DATA_DIR = os.path.abspath(DATA_DIR) # Make sure path is absolute so that symlinks work
-
-    ## Configuration Space ##
-    ## Define parameters to select models
     os.mkdir(WORKING_DIR)
-
-    ############################
-    ### Gather sk_models,  aleph probability_class, good_rule and bad_rule models
-    #############################
-    # candidate_models = CandidateModels()
-    # candidate_models.load_sk_folder(os.path.join(DATA_DIR,PARTIAL_GROUNDING_RULES_DIR))
-    # candidate_models.load_aleph_folder(os.path.join(DATA_DIR, PARTIAL_GROUNDING_ALEPH_DIR))
 
     ############################
     ### Create model parameters
@@ -321,29 +150,8 @@ def run_smac(ROOT, DATA_DIR, WORKING_DIR, domain_file, instance_dir, instances_w
     optimizer = Categorical('optimizer', ['Adam', 'RMSprop'], default='Adam')
     lr = Categorical('lr', [0.001, 0.01, 0.005], default=0.001)
 
-    # alias = Categorical ('alias', ['lama-first'], default='lama-first')
-    # queue_type = Categorical("queue_type", ["trained", "roundrobintrained", "fifo", "lifo"], default='trained')
-
     parameters = [target_folder, layers_num, hidden_size, conv_type, aggr, optimizer, lr]
-    # conditions = []
-
-    # for schema, models in candidate_models.sk_models_per_action_schema.items():
-    #     m = Categorical(f"model_{schema}", models)
-    #     parameters.append(m)
-    #     conditions.append(InCondition(child=m, parent=queue_type, values=["trained", "roundrobintrained"]))
-
-    # for i, r in enumerate(candidate_models.good_rules):
-    #     parameters.append(Categorical(f"good_{i}", [False, True]))
-
-    # for i, r in enumerate(candidate_models.bad_rules):
-    #     parameters.append(Categorical(f"bad{i}", [False, True]))
-
-    ############################
-    ### Stopping condition parameters
-    #############################
-
-    # TODO!!
-
+ 
 
 
 
@@ -369,7 +177,13 @@ def run_smac(ROOT, DATA_DIR, WORKING_DIR, domain_file, instance_dir, instances_w
     smac = HyperparameterOptimizationFacade(scenario, evaluator.target_function)
     incumbent = smac.optimize()
 
-    print("Chosen configuration: ", incumbent)
+    # print("Chosen configuration: ", incumbent)
+
+    config = parse_config(incumbent)
+    print("Chosen configuration: ", config)
+
+
+
     # if 'trained' in  incumbent['queue_type']:
     #     candidate_models.copy_model_to_folder(incumbent, os.path.join(WORKING_DIR, 'incumbent'), symlink=False )
     # else:
